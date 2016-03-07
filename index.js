9bf97a6a9ff43fa5053dd1e4d0f36a0061cb6710
Brian Kemper <bkemper@gmail.com>
2016-03-07 13:32:50 -0700

    got it working

--- index.js
+++ index.js
@@ -1,33 +1,66 @@
-var express=require('express');
+var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+//noinspection JSUnresolvedFunction
 var mongojs = require('mongojs');
 var db = mongojs('birds');
 var port = 8080;
 var collection = db.collection('sightings');
 var app = express();
+var ObjectID = require('mongodb').ObjectID;
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/api/sighting', function(req, res, next){
-    console.log(req.body);
-    res.status(200).json('post');
+    collection.insert(req.body, function(err, resp){
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.status(200).json(resp);
+        }
+    });
+
 });
 
 app.get('/api/sighting', function(req, res, next){
-    console.log(req.body);
-    res.status(200).json('get');
+    var name = req.query.name;
+    collection.find({name: name}, function(err, resp){
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.status(200).json(resp);
+        }
+    });
+
 });
 
 app.put('/api/sighting', function(req, res, next){
-    console.log(req.body);
-    res.status(200).json('put');
+    var id = req.query.id;
+    collection.update({'_id' : ObjectID(id)}, {
+        name: req.body.name,
+        order: req.body.order,
+        status: req.body.status
+    }, function(err, resp){
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.status(200).json(resp);
+        }
+    });
+
 });
 
 app.delete('/api/sighting', function(req, res, next){
-    console.log(req.body);
-    res.status(200).json('del');
+    var id = req.query.id;
+    collection.remove({'_id' : ObjectID(id)}, function(err, resp) {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.status(200).json(resp);
+        }
+    });
+
 });
 
 app.listen(port, function(){
